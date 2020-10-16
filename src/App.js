b6258e4ae0c2255d461e14d187ed01bb82603341
Jason <jason.huang.2@stonybrook.edu>
2020-10-16 18:23:14 -0400

    prepping client

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./components/pages/login"
+import Home from "./components/pages/home"
+
+let checkUserStatus = () => {
+    return null;
+};
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { 
+            apiResponse: "" 
+        };
+        this.user = checkUserStatus();
     }
-
+    
     callAPI() {
         fetch("http://localhost:42069/testAPI")
             .then(res => res.text())
@@ -16,17 +25,17 @@ class App extends Component {
     }
 
     componentDidMount() {
-        this.callAPI();
     }
 
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
+                {
+                this.user ? 
+                <Home></Home> : <Login user={user}></Login>
+                }
             </div>
         );
     }
---
+++ src/components/pages/home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+
+export function Home(props) {
+
+
+	return (
+		<div>
+			
+		</div>
+	);
+}
