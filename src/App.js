2a029c550c127c1559c29ee17e2000c563455f3a
Jason <jason.huang.2@stonybrook.edu>
2020-10-23 16:16:36 -0400

    adding imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/pages/Login"
 import Home from "./components/pages/Home"
+import SignUp from "./components/pages/SignUp"
+import About from "./components/pages/About"
+import ForgotPassword from "./components/pages/ForgotPassword"
+import MyPlaylist from "./components/pages/MyPlaylist"
+import Matches from "./components/pages/Matches"
+import Settings from "./components/pages/Settings"
+import Notifications from "./components/pages/Notifications"
 
 let checkUserStatus = () => {
     return null;
@@ -14,7 +22,8 @@ class App extends Component {
         this.state = { 
             apiResponse: "app not working" 
         };
-        this.user = checkUserStatus();
+        // this.user = checkUserStatus();
+        // this.user = False;
     }
     
     callAPI() {
@@ -33,12 +42,23 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    {this.state.apiResponse}
-                </header>
-                {this.user ? <Home></Home> : <Login user={this.user}></Login>}
-            </div>
+            <BrowserRouter>
+                {/* {user ? 
+                <Redirect exact from="/" to={{ pathname: "/home" }} /> : <Redirect exact from="/" to={{ pathname: "/login" }} />} */}
+				<Route path="/login" name="login" render={(props) =>
+					<Login/>}
+				/>
+				<Route />
+				<Route path="/register" name="register" render={(props) =>
+					<SignUp/>}
+				/>
+				<Route />
+
+				<Route path="/home" name="home" render={(props) =>
+					<Homescreen/>}
+				/>
+				<Route />
+            </BrowserRouter>
         );
     }
 }
