aaa3130f58ce5dde45a6324df494ba3c6b8feee1
Darren Kong <darren.kong@stonybrook.edu>
2020-10-27 21:20:23 -0400

    Finished notifications page

---
+++ src/components/modules/Notification.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function Notification(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  return (
+    <div className={classes.root}>
+      <Collapse in={open}>
+        <Alert color='info'
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="large"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          <Link component='a' href={props.link}>{props.message}</Link>
+        </Alert>
+      </Collapse>
+    </div>
+  );
+}
--- src/components/pages/Notifications.js
+++ src/components/pages/Notifications.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from '../navbar/Sidebar';
+import { makeStyles } from '@material-ui/core/styles';
+import { Alert, AlertTitle } from '@material-ui/lab';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
+import Notification from '../modules/Notification';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  width: '100%',
+	  '& > * + *': {
+		marginTop: theme.spacing(2),
+	  },
+	},
+  }));
+
 
 function Notifications(props) {
+	const classes = useStyles();
 	return (
 		<div>
 			<Sidebar pageName='Notifications'></Sidebar>
+			<Notification message='Hello There'></Notification>
+			<Notification link='http://localhost:8080/matches' message='JSON has sent you a message!'></Notification>
+			<Notification link='http://localhost:8080/settings' message='FARHAN has matched with you!'></Notification>
+			<Notification link='http://localhost:8080/myplaylists' message='DARREN has commented on your playlist!'></Notification>
 		</div>
 	);
 }
