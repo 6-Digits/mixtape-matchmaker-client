e5f79b7d2d6d7dfda73af02da515c93cc2bbe46a
Jason <jason.huang.2@stonybrook.edu>
2020-12-07 17:47:47 -0500

    pushing notification socket

---
+++ src/components/frameworks/NotificationSocket.js
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from "react";
+import socketIOClient from "socket.io-client";
+
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+const SOCKET_SERVER_URL = "http://localhost:5000";
+
+const NotifcationSocket = (roomId, user) => {
+	const [notifications, setNotifications] = useState([]);
+	const socketRef = useRef();
+
+	useEffect(() => {
+		socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+			query: { roomId: user['_id'] },
+		});
+
+		setNotifications([]);
+
+		socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (message) => {
+			//alert(JSON.stringify(message))
+			const incomingMessage = {
+				...message,
+				ownedByCurrentUser: message.senderId === socketRef.current.id,
+				chatID : roomId,
+			};
+			setMessages((messages) => [...messages, incomingMessage]);
+			//setMessages([incomingMessage])
+		});
+
+		return () => {
+			socketRef.current.disconnect();
+		};
+	}, [roomId]);
+	
+	// The function below notifies the server of a new message
+	const sendMessage = (messageBody) => {
+		socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+			user: user,
+			body: messageBody,
+			date: new Date().toISOString(),
+			senderId: socketRef.current.id,
+		});
+	};
+
+	return { messages, sendMessage };
+};
+
+export default NotifcationSocket;
