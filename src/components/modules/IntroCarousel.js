22041b423bb95c65336060efad7f958fd6156da0
Jason <jason.huang.2@stonybrook.edu>
2020-12-10 15:45:22 -0500

    login carousel optimized

--- src/components/modules/IntroCarousel.js
+++ src/components/modules/IntroCarousel.js
@@ -12,24 +12,50 @@ import logo from "../../assets/logo.png";
 const useStyles = makeStyles((theme)=>({
     carousel: {
         width: "100%",
+        zIndex: 9999
     },
     item: {
         height: "100vh",
         margin: "auto",
     },
     headphones: {
-
+        backgroundImage: `url(${headphones})`, 
+        backgroundPosition: 'center',
+        backgroundSize: "cover",
+        height: "100%",
+        width: `${(8/12) * 100}%`,
+        position: 'absolute',
+        top: 0
     },
     share: {
+        backgroundImage: `url(${share})`, 
+        backgroundPosition: 'center',
+        backgroundSize: "cover",
+        height: "100%",
+        width: `${(8/12) * 100}%`,
+        position: 'absolute',
+        top: 0
 
     },
     container: {
     },
     connect: {
-
+        backgroundImage: `url(${connect})`, 
+        backgroundPosition: 'center',
+        backgroundSize: "cover",
+        height: "100%",
+        width: `${(8/12) * 100}%`,
+        position: 'absolute',
+        top: 0
     },
     mm:{
-
+        backgroundImage: `url(${rings})`, 
+        backgroundPosition: 'center',
+        backgroundSize: "cover",
+        width: `${(8/12) * 100}%`,
+        height: "100%",
+        position: 'absolute',
+        top: 0
     },
     logo: {
         width: "20rem",
@@ -41,23 +67,23 @@ const useStyles = makeStyles((theme)=>({
     }
 }));    
 function IntroCarousel(props){
-    const images = [headphones, share, connect, rings];
-    const [image, setImage] = useState(headphones);
+    const [carouselIndex, setCarouselIndex] = useState(0);
 
     const onAnimate = (index, active) => {
-        setImage(images[index]);
+        setCarouselIndex(index);
     }
 
 	const classes = useStyles();
     return (
         <Grid container className={classes.container} alignItems="center" 
-            style={
-                {
-                    backgroundImage: `url(${image})`, 
-                    backgroundPosition: 'center',
-                    backgroundSize: "cover"
-                }
-                }>
+            // style={
+            //     {
+            //         backgroundImage: `url(${image})`, 
+            //         backgroundPosition: 'center',
+            //         backgroundSize: "cover"
+            //     }
+            //     }
+            >
             <Carousel
             interval={3000}
             animatiom="slide"
@@ -65,22 +91,38 @@ function IntroCarousel(props){
             className={classes.carousel}
             onChange={onAnimate}
             navButtonsAlwaysVisible={true}>
-                <Grid direction="column" container justify="center" alignItems="center" className={classes.headphones}>
+                <Grid direction="column" container justify="center" alignItems="center" >
                     <Typography variant="h1">{"Listen"}</Typography>
                 </Grid> 
-                <Grid direction="column" container justify="center" alignItems="center" className={classes.share}>
+                <Grid direction="column" container justify="center" alignItems="center" >
                     <Typography variant="h1">{"Share"}</Typography>
                 </Grid> 
-                <Grid direction="column" container justify="center" alignItems="center" className={classes.connect}>
+                <Grid direction="column" container justify="center" alignItems="center" >
                     <Typography variant="h1">{"Connect"}</Typography>
                 </Grid> 
-                <Grid direction="column" container justify="center" alignItems="center" className={classes.mm}>
+                <Grid direction="column" container justify="center" alignItems="center" >
                     <Avatar variant="square" src={logo} className={classes.logo}></Avatar>
                     <Typography variant="h1">{"Mixtape Matchmaker"}</Typography>
                     <Typography variant="h3">{"Make More Memories"}</Typography>
                     <Button href="/about" variant="contained" className={classes.button}>{"Learn more"}</Button>
                 </Grid> 
             </Carousel>
+            {
+                carouselIndex === 0 ?
+                <div className={classes.headphones}></div> : null
+            }
+            {
+                carouselIndex === 1 ?
+                <div className={classes.share}></div> : null
+            }
+            {
+                carouselIndex === 2 ?
+                <div className={classes.connect}></div> : null
+            }
+            {
+                carouselIndex === 3 ?
+                <div className={classes.mm}></div> : null
+            }
         </Grid>
         
     )
